dcdfcb53f32e50366a8d18dab36e1a601c404f2e
mkrul <mishakrul@gmail.com>
2019-02-27 07:13:03 -0500

    TODO-5 add tests for new_status method

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
+  context '#new_status' do
+    setup do
+      @pending_task = Task.new(status: 'pending')
+      @completed_task = Task.new(status:'complete')
+    end
+
+    should "return 'complete' if status is 'pending'" do
+      new_status = @pending_task.new_status
+      assert_equal 'complete', new_status
+    end
+
+    should "return 'pending' if status is 'complete'" do
+      new_status = @completed_task.new_status
+      assert_equal 'pending', new_status
+    end
+  end
 end
