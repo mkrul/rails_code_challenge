6827d2f3733e306d79142b668c372786350f3462
mkrul <mishakrul@gmail.com>
2019-02-27 08:09:40 -0500

    TODO-6 run migration to add completed_at to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190227113847) do
+ActiveRecord::Schema.define(version: 20190227130729) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20190227113847) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "pending", null: false
+    t.datetime "completed_at"
   end
 
 end
