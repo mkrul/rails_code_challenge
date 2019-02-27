08ccf31716c5cec05d3a4960b8520b76cd3f625c
mkrul <mishakrul@gmail.com>
2019-02-27 06:55:12 -0500

    TODO-5 add new_status method, route to toggle_completion, show status in index view

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ApplicationRecord
+
+  STATUS_COMPLETE = 'complete'.freeze
+  STATUS_PENDING = 'pending'.freeze
+
+  def new_status
+    status == STATUS_COMPLETE ? STATUS_PENDING : STATUS_COMPLETE
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  
   resources :tasks
+  put '/tasks/:id/toggle_completion', to: 'tasks#toggle_completion', as: 'toggle_completion'
 end
