b3f573a9820ddf5645abcf81a7e8ffc85dff5042
mkrul <mishakrul@gmail.com>
2019-02-27 06:41:48 -0500

    TODO-5 run migration to add status column to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190227112654) do
+ActiveRecord::Schema.define(version: 20190227113847) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "pending", null: false
   end
 
 end
